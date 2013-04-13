a152701669e946be77464ce46209503b9018c7c0
liyiqiang <liyiqiang@mail.nwpu.edu.cn>
2013-04-13 19:58:26 +0800

    添加了一个测试读入数据文件的单元测试用例。

--- bbfmm_tests/test_mesh.cpp
+++ bbfmm_tests/test_mesh.cpp
@@ -3,13 +3,16 @@
 //
 #include "cppunit/extensions/HelperMacros.h"
 
+#include "mesh.h"
+#include <string>
+
 class MeshTest: public CppUnit::TestFixture
 {
 
 	CPPUNIT_TEST_SUITE( MeshTest );
 	
 	
-	CPPUNIT_TEST( test_read );
+	CPPUNIT_TEST( test_load_mesh );
 	
 	
 	CPPUNIT_TEST_SUITE_END();
@@ -23,7 +26,20 @@ class MeshTest: public CppUnit::TestFixture
 	void tearDown();
   
 	// 测试用例
-	void test_read();
+	int test_load_mesh() 
+	{
+		Mesh mesh2d,mesh3d;
+		mesh2d.load_mesh(std::string("F:\\LiYiQiang\\Desktop\\Fast Bem\\code\\cbem_new\\2d.dat"));
+		mesh2d.plot_mesh(std::string("F:\\LiYiQiang\\Desktop\\Fast Bem\\code\\cbem_new\\2d.dat") + ".plt");
+
+		//mesh2d.load_mesh(std::string("e:\\bem2d_big.dat"));
+		//mesh2d.plot_mesh(std::string("e:\\bem2d_big.dat") + ".plt");
+
+		mesh3d.load_mesh(std::string("F:\\LiYiQiang\\Desktop\\Fast Bem\\code\\cbem_new\\3d.dat"));
+		mesh3d.plot_mesh(std::string("F:\\LiYiQiang\\Desktop\\Fast Bem\\code\\cbem_new\\3d.dat") + ".plt");
+		return 1;
+	}
+}
 };
 
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( MathTest, "alltests" );
